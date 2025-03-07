360992509dd25f54c5403d62ecaeda40650a5c33
tokencenter <202347285+tokencenter@users.noreply.github.com>
2025-03-08 00:54:15 +0100

    up

--- .vuepress/mixins/tokenDetails.js
+++ .vuepress/mixins/tokenDetails.js
@@ -35,7 +35,7 @@ export default {
           tokenRecover: false,
           removeCopyright: false,
           originalPrice: 0.5,
-          price: 0.5,
+          price: 0.1,
           gas: 1333276,
         },
         {
@@ -53,7 +53,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: .75,
-          price: 0.75,
+          price: 0.15,
           gas: 1260139,
         },
         {
@@ -71,7 +71,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1,
-          price: 1,
+          price: .2,
           gas: 1444650,
         },
         {
@@ -89,7 +89,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1,
-          price: 1,
+          price: .2,
           gas: 1524151,
         },
         {
@@ -107,7 +107,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1.25,
-          price: 1.25,
+          price: .25,
           gas: 1708581,
         },
         {
@@ -125,7 +125,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1.5,
-          price: 1.5,
+          price: .3,
           gas: 1637163,
         },
         {
@@ -143,7 +143,7 @@ export default {
           tokenRecover: true,
           removeCopyright: true,
           originalPrice: 2,
-          price: 2,
+          price: .4,
           gas: 2254686,
         },
       ],
